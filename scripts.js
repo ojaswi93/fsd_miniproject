582cb113e2caee5eb04af03c6b81bfa1fd3cc8f2
ojaswi93 <ojaswi.endait@gmail.com>
2024-08-26 09:33:00 +0530

    File Organisation
    Separated Javascript file and included worker-home-html, made a different container called main content so that new content doesn't overlap with header and sidebar.

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menu-btn");
+    const sidebar = document.getElementById("sidebar");
+    const mainContent = document.getElementById("main-content");
+  
+    menuBtn.onclick = function () {
+      sidebar.classList.toggle("collapsed");
+  
+      if (sidebar.classList.contains("collapsed")) {
+        mainContent.style.marginLeft = 'var(--sidebar-width-collapsed)';
+      } else {
+        mainContent.style.marginLeft = 'var(--sidebar-width)';
+      }
+    };
+  });
+  
