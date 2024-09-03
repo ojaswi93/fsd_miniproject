179e533568d6c9aa14ffccf95c8fe754d700bc29
Dhanshribhangale <dhanshribhangale67@gmail.com>
2024-09-03 13:31:10 +0530

    Resolving conflicts

---
+++ my-fsd-project/src/Header.jsx
@@ -0,0 +1,15 @@
+import logopic from './assets/logo.png'
+
+function Header() {
+    return (
+        <div id="header">
+            <img id="logo" src={logopic} alt="Logo" />
+            <div id="header-container">
+                <p id="header-text1">Worksapp</p>
+                <p id="header-text2">Where work finds you</p>
+            </div>
+        </div>);
+
+}
+
+export default Header
