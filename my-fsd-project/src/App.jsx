e08cde8851b0084aa8c102e66bc4030e1f980dbf
Ravija-Pasalkar <ravijapasalkar1@gmail.com>
2024-09-10 14:03:28 +0530

    Company Registration Page added

--- my-fsd-project/src/App.jsx
+++ my-fsd-project/src/App.jsx
@@ -5,11 +5,12 @@ import './App.css'
 import './index.css'
 import Header from './Header.jsx'
 import HomePage from './HomePage.jsx'
+import EmployerRegistration from './EmployerRegistration.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <EmployerRegistration />
     </div>
   );
 }
