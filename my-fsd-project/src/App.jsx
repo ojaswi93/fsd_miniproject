f841f3d8a8e9d069481c14620bd60ac93c3b30d7
Ravija-Pasalkar <ravijapasalkar1@gmail.com>
2024-09-10 19:12:16 +0530

    Navigation from home page to Employer Registration Page added

--- my-fsd-project/src/App.jsx
+++ my-fsd-project/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,10 +11,13 @@ import EmployerRegistration from './EmployerRegistration.jsx'
 
 function App() {
   return (
-    <div>
-      <EmployerRegistration />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Employer-Registration" element={<EmployerRegistration />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
