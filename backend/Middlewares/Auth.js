949387d9d677fb2fa76db02d4704f1e002a1cc56
ojaswi93 <ojaswi.endait@gmail.com>
2024-10-12 17:56:21 +0530

    Backend connectivity and Employee registration
    Data submitted in the form reflects in MongoDB, Login page pending

---
+++ backend/Middlewares/Auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const ensureAuthenticated = (req, res, next) => {
+  const auth = req.headers["authorization"];
+  if (!auth) {
+    return res
+      .status(403)
+      .json({ message: "Unauthorised , JWT token is required" });
+  }
+  try {
+    const decoded = jwt.verify(auth, process.env.JWT_SECRET);
+    re.user = decoded;
+    next();
+  } catch (err) {
+    return res
+      .status(403)
+      .json({ message: "Unauthorised , JWT token is wrong or expired" });
+  }
+};
+
+module.exports = ensureAuthenticated;
--- my-fsd-project/src/EmployeeRegistration.jsx
+++ my-fsd-project/src/EmployeeRegistration.jsx
@@ -2,54 +2,68 @@ import React, { useState } from "react";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { handleError } from "./utils";
+import { handleSuccess } from "./utils";
 
 const EmployeeRegistration = () => {
   const navigate = useNavigate();
 
-  const [formData, setFormData] = useState({
-    companyName: "",
-    location: "",
-    username: "",
+  const [signupInfo, setSignupInfo] = useState({
+    name: "",
     email: "",
     password: "",
-    confirm_password: "",
   });
 
   const [showPassword, setShowPassword] = useState(false);
 
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-
-  const [errorMessage, setErrorMessage] = useState("");
-
   const handleChange = (e) => {
-    const { id, value } = e.target;
-    setFormData({ ...formData, [id]: value });
+    const { name, value } = e.target;
+    console.log(name, value);
+    const copySignupInfo = { ...signupInfo };
+    copySignupInfo[name] = value;
+    setSignupInfo(copySignupInfo);
   };
-
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const toggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
-
-  const handleRedirection = () => {
-    console.log("Navigating to login page");
-    navigate("/Login");
-  };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Form Data Submitted: ", formData);
-
-    if (formData.password !== formData.confirm_password) {
-      setErrorMessage("Passwords do not match!");
-      return;
+    console.log("Form Data Submitted: ", signupInfo);
+    const { name, email, password } = signupInfo;
+    if (!name || !email || !password) {
+      return handleError("name, email and password are required");
     }
-    setErrorMessage("");
+    try {
+      const url = "http://localhost:8080/auth/signup";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signupInfo),
+      });
 
-    handleRedirection();
+      const result = await response.json();
+      console.log(result);
+      const { success, message, error } = result;
+
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate("/Login");
+        }, 1000);
+      } else if (error) {
+        const details = error?.details[0].message;
+        handleError(details);
+      } else if (!success) {
+        handleError(nessage);
+      }
+      console.log(result);
+    } catch (err) {
+      handleError(err);
+    }
   };
 
   const handleLogin = () => {
@@ -63,48 +77,17 @@ const EmployeeRegistration = () => {
           <h2 className="login-title">Register Yourself</h2>
           <form onSubmit={handleSubmit}>
             <div className="input-box">
-              <label htmlFor="firstName" className="input-label">
-                First Name
+              <label htmlFor="name" className="input-label">
+                Name
               </label>
               <input
                 type="text"
-                id="firstName"
-                name="firstName"
+                name="name"
                 className="login-input"
-                placeholder="Enter your first name"
-                value={formData.firstName}
+                placeholder="Enter your name"
+                value={signupInfo.name}
                 onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="input-box">
-              <label htmlFor="lastName" className="input-label">
-                Last Name
-              </label>
-              <input
-                type="text"
-                id="lastName"
-                name="lastName"
-                className="login-input"
-                placeholder="Enter your last name"
-                value={formData.lastName}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="input-box">
-              <label htmlFor="username" className="input-label">
-                Username
-              </label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                className="login-input"
-                placeholder="Enter your username"
-                value={formData.username}
-                onChange={handleChange}
-                required
+                //required
               />
             </div>
             <div className="input-box">
@@ -113,13 +96,12 @@ const EmployeeRegistration = () => {
               </label>
               <input
                 type="email"
-                id="email"
                 name="email"
                 className="login-input"
                 placeholder="example@gmail.com"
-                value={formData.email}
+                value={signupInfo.email}
                 onChange={handleChange}
-                required
+                //required
               />
             </div>
             <div className="input-box">
@@ -129,13 +111,12 @@ const EmployeeRegistration = () => {
               <div style={{ position: "relative" }}>
                 <input
                   type={showPassword ? "text" : "password"}
-                  id="password"
                   name="password"
                   className="login-input"
                   placeholder="Password"
-                  value={formData.password}
+                  value={signupInfo.password}
                   onChange={handleChange}
-                  required
+                  //required
                 />
                 <i
                   className={`fa ${showPassword ? "fa-eye-slash" : "fa-eye"}`}
@@ -150,36 +131,7 @@ const EmployeeRegistration = () => {
                 />
               </div>
             </div>
-            <div className="input-box">
-              <label htmlFor="confirm_password" className="input-label">
-                Confirm Password
-              </label>
-              <div style={{ position: "relative" }}>
-                <input
-                  type={showConfirmPassword ? "text" : "password"}
-                  id="confirm_password"
-                  name="confirm_password"
-                  className="login-input"
-                  placeholder="Confirm Password"
-                  value={formData.confirm_password}
-                  onChange={handleChange}
-                  required
-                />
-                <i
-                  className={`fa ${
-                    showConfirmPassword ? "fa-eye-slash" : "fa-eye"
-                  }`}
-                  onClick={toggleConfirmPasswordVisibility}
-                  style={{
-                    position: "absolute",
-                    right: "10px",
-                    top: "50%",
-                    transform: "translateY(-50%)",
-                    cursor: "pointer",
-                  }}
-                />
-              </div>
-            </div>
+
             <button type="submit" className="login-btn">
               Register
             </button>
@@ -191,6 +143,7 @@ const EmployeeRegistration = () => {
               </button>
             </p>
           </form>
+          <ToastContainer></ToastContainer>
         </div>
 
         <div className="login-right"></div>
