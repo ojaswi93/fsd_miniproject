2f8c30eee538bc2795e5de968042ca48329c6666
ojaswi93 <ojaswi.endait@gmail.com>
2024-10-28 20:54:14 +0530

    Changed employee schema

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -5,6 +5,12 @@ const EmployeeSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String,
+  lastname: { type: String, default: "" },
+  location: { type: String, default: "" },
+  gender: { type: String, default: "" },
+  age: { type: Number, default: null },
+  aadhar: { type: String, default: "" },
+  about: { type: String, default: "" },
 });
 
 const EmployeeModel = mongoose.model("employees", EmployeeSchema);
